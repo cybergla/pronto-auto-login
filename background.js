d68b5d119258dd8482dfaa57465763fed46aba50
cybergla <tanaydeshmukh@gmail.com>
2015-09-08 22:06:04 +0530

    added bw logo

--- background.js
+++ background.js
@@ -1,11 +1,55 @@
 chrome.runtime.onStartup.addListener(function () {
-  /*chrome.storage.sync.get(null,function(data){
+  chrome.browserAction.setIcon({
+          path: './icon16.png'
+      });
+  chrome.storage.sync.get(null,function(data) {
     var username = data.username;
     var password = data.password;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("POST","http://phc.prontonetworks.com/cgi-bin/authlogin",true);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send("userId="+username+"&password="+password+"&serviceName=ProntoAuthentication&Submit22=Login");
-    */
+    xmlhttp.onreadystatechange = function () {
+      if ( 4 != xmlhttp.readyState ) {
+          return;
+      }
+      if ( 200 != xmlhttp.status ) {
+          return;
+      }
+      var patt_success = /congratulations/i;
+      var patt_already = /already logged in/i;
+      var patt_quota_over = /quota is over/i;
+      var patt_error = /sorry/i;
+
+      var login_success = patt_success.test(xmlhttp.responseText)
+      var login_error = patt_error.test(xmlhttp.responseText);
+      var quota_over = patt_quota_over.test(xmlhttp.responseText);
+      var already_logged_in = patt_already.test(xmlhttp.responseText);
+
+      if(login_success){
+        setTimeout(function() { alert("Logged in successfully"); }, 700);
+      }
+      if(login_error){
+        setTimeout(function() { alert("Incorrect credentials"); }, 700);
+      }
+      if(quota_over){
+        setTimeout(function() { alert("Your quota is over"); }, 700);
+      }
+      if(already_logged_in){
+        //setTimeout(function() { alert("Already logged in"); }, 700); //annoying, no need
+        console.log("Already Logged in");
+      }
+    };
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse){
+    if(request.setIconColor == true){
+      chrome.browserAction.setIcon({path: './icon16.png'});
+    }
+    if(request.setIconBw ==  true){
+      chrome.browserAction.setIcon({path: './ic_pronto_bw_16.png'});
+    }
+  }
+);
--- popup.js
+++ popup.js
@@ -1,17 +1,19 @@
+function setStatus(markup) {
+  document.getElementById('status').innerHTML = '<h4>' + markup + '</h4>';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var loginButton = document.getElementById('login');
   var submitButton =  document.getElementById('submit_form');
+  var logoutButton = document.getElementById('logout');
   //get data from storage
   chrome.storage.sync.get(null,function(data){
     document.getElementById('username').value = data.username;
     document.getElementById('password').value = data.password;
   });
 
-  function renderHtml(markup) {
-    document.getElementById('content').innerHTML = markup;
-  }
-
-  loginButton.addEventListener('click',function login() {
+  loginButton.addEventListener('click', function login() {
+    setStatus('logging in, please wait...');
     chrome.storage.sync.get(null,function(data){
       var username = data.username;
       var password = data.password;
@@ -26,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if ( 200 != xmlhttp.status ) {
             return;
         }
-        console.log(xmlhttp.responseText);
+        //console.log(xmlhttp.responseText);
         var patt_success = /congratulations/i;
         var patt_already = /already logged in/i;
         var patt_quota_over = /quota is over/i;
@@ -38,27 +40,31 @@ document.addEventListener('DOMContentLoaded', function() {
         var res4 = patt_already.test(xmlhttp.responseText);
 
         if(res1){
-          renderHtml('<h2>Login Successful</h2>');
+          setStatus('Login Successful');
+          chrome.runtime.sendMessage({'setIconColor':true});
           console.log("logged in");
         }
         if(res2){
-          renderHtml('<h2>Incorrect credentials</h2>');
+          setStatus('Incorrect credentials');
           console.log("Incorrect username/pass");
         }
         if(res3){
-          renderHtml('<h2>Your quota is over</h2>');
+          setStatus('Your quota is over');
           console.log("quota over");
         }
         if(res4){
-          renderHtml('<h2>Already Logged In</h2>');
+          setStatus('Already Logged In');
           console.log("already logged in");
+        }
+
+        if(!res1 && !res2 && !res3 && !res4){
+          setStatus('Unknown error occurred :(');
+          console.log("Unkown error");
         }
       };
     });
   });
-
-  //logout
-  document.getElementById('logout').addEventListener('click',function () {
+  logoutButton.addEventListener('click',function logout() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET","http://phc.prontonetworks.com/cgi-bin/authlogout",true);
     xmlhttp.send();
@@ -66,16 +72,21 @@ document.addEventListener('DOMContentLoaded', function() {
     {
       if (xmlhttp.readyState==4 && xmlhttp.status==200)
       {
-        var patt_loggedout = new RegExp(/successfully logged out/i);
-        var res1 = patt_loggedout.test(xmlhttp.responseText);
-        if(res1)
-        {
-          renderHtml('<h2>Logout Successful</h2>');
+        var patt_logout = new RegExp(/successfully logged out/i);
+        var patt_no_active = new RegExp(/no active session/i);
+
+        var res1 = patt_logout.test(xmlhttp.responseText);
+        var res2 = patt_no_active.test(xmlhttp.responseText);
+        if(res1){
+          setStatus('Logout Successful');
         }
-      else if(!res1)
-        {
-          renderStatus('<h2>Could not logout</h2>');
+        if(res2){
+          setStatus('No active session exists');
         }
+        else if(!res1 && !res2){
+          setStatus('Could not logout');
+        }
+        chrome.runtime.sendMessage({'setIconBw':true});
       }
     }
   });
@@ -86,9 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
     username= f.elements['username'].value;
     password= f.elements['password'].value;
     chrome.storage.sync.set({"username": username ,"password": password}, function () {
-      document.getElementById('status').innerHTML = '<h4>Saved login data</h4>';
-      setInterval(function () {document.getElementById('status').innerHTML = '';}, 1500);
-      console.log("settings saved");
+      setStatus('Saved login data');
     });
   });
-  }, false);
+}, false);
